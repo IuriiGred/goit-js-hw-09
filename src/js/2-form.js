7a56e92ca4eb5044fb14c4528be8105d5f402540
IuriiGred <gredasoviurii@gmail.com>
2025-06-01 20:52:43 +0200

    fourth commit

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,7 +3,6 @@ const formData = {
     message: "",
 }
 const lokalKey = "feedback-form-state";
-
 const handleFormEl = document.querySelector('.feedback-form');
 
 handleFormEl.addEventListener('input', onInput);
@@ -15,15 +14,13 @@ function onInput(event) {
   localStorage.setItem(lokalKey, JSON.stringify(obj))
 }
 
-const loadFormValue = () => {
-  const replaceValue = localStorage.getItem(lokalKey);
+const replaceValue = JSON.parse(localStorage.getItem(lokalKey));
 
-  if (replaceValue !== null) {
-    const formStorage = JSON.parse(replaceValue);
-    
-    handleFormEl.elements.email.value = formStorage.email;
-    handleFormEl.elements.message.value = formStorage.message;
-  }
+if (replaceValue !== null) {
+  const newObj = { ...formData, ...replaceValue}
+  
+  handleFormEl.elements.message.value = newObj.message;
+  handleFormEl.elements.email.value = newObj.email;
 };
 
 handleFormEl.addEventListener('submit', onSubmit);
@@ -50,77 +47,3 @@ function onSubmit(event){
 
   handleFormEl.reset()
 }
-
-loadFormValue();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const handleFormEl = document.querySelector('.feedback-form');
-// const handleInputEl = document.querySelector('input');
-// const handleTextareaEl = document.querySelector('textarea');
-
-// function saveInputFields() {
-//     const savedValues = {
-//         email: handleInputEl.value,
-//         message: handleTextareaEl.value,
-//     };
-//     localStorage.setItem(lokalKey, JSON.stringify(savedValues));
-// };
-
-// handleFormEl.addEventListener('input', saveInputFields);
-
-// const loadFormValue = () => {
-//   const replaceValue = localStorage.getItem(lokalKey);
-//   if (replaceValue !== null) {
-//     const formStorage = JSON.parse(replaceValue);
-//     handleInputEl.value = formStorage.email;
-//     handleTextareaEl.value = formStorage.message;
-//   }
-// };
-
-// const clearFormValue = () => {
-//   localStorage.removeItem(lokalKey);
-
-//   handleInputEl.value = '';
-//   handleTextareaEl.value = '';
-// };
-
-// handleFormEl.addEventListener('submit', onSubmit);
-
-// function onSubmit(evt) {
-//   evt.preventDefault();
-//     formData.email =  handleInputEl.value;
-//     formData.message = handleTextareaEl.value;
-
-// if(!handleInputEl.value || !handleTextareaEl.value) {
-//   alert("Fill please all fields");
-//   return;
-// }
-
-// console.log(formData);
-
-// clearFormValue();
-// };
-
-// loadFormValue();

220ec72ed01cbaea5ae1f9725173df77340a706f
IuriiGred <gredasoviurii@gmail.com>
2025-06-02 13:07:54 +0200

    fifth attempt

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,44 +1,37 @@
-const formData = {
-    email: "",
-    message: "",
-}
-const lokalKey = "feedback-form-state";
+let formData = {
+  email: '',
+  message: '',
+};
+
 const handleFormEl = document.querySelector('.feedback-form');
+
+const lokalKey = 'feedback-form-state';
 
 handleFormEl.addEventListener('input', onInput);
 
-const obj = {};
 function onInput(event) {
   const {name, value} = event.target;
-  obj[name] = value.trim();
-  localStorage.setItem(lokalKey, JSON.stringify(obj))
+    formData[name] = value.trim();
+    localStorage.setItem(lokalKey, JSON.stringify(formData));
 }
 
 const replaceValue = JSON.parse(localStorage.getItem(lokalKey));
-
-if (replaceValue !== null) {
-  const newObj = { ...formData, ...replaceValue}
-  
-  handleFormEl.elements.message.value = newObj.message;
-  handleFormEl.elements.email.value = newObj.email;
-};
+if (replaceValue) {
+  formData = { ...formData, ...replaceValue };
+  handleFormEl.elements.email.value = formData.email;
+  handleFormEl.elements.message.value = formData.message;
+}
 
 handleFormEl.addEventListener('submit', onSubmit);
 
 function onSubmit(event){
   event.preventDefault();
-  const savedSetting = localStorage.getItem(lokalKey);
-  const parceSetting = JSON.parse(savedSetting);
-
-  const {email, message} = parceSetting;
-
-  if(!email || !message){
+  
+  if(!event.target.email.value || !event.target.message.value){
     alert("Fill please all fields");
   return;
   }
 
-  formData.email = email;
-  formData.message = message;
   console.log(formData);
   
   formData.email = "";
